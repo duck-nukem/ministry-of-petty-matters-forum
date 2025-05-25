3302fbde3509572a82b1a67b741e1d931f939f6a
Alex Szabo <kreatemore@gmail.com>
2025-05-25 17:16:16 +0200

    remove unnecessary prefix

--- src/config.rs
+++ src/config.rs
@@ -17,9 +17,10 @@ impl Config {
 static DEVELOPMENT_ENCRYPTION_KEY: &str = "czNjcjN0LXMzY3IzdC1zM2NyM3QtczNjcjN0LXMzY3IzdA==";
 
 pub static APP_CONFIG: LazyLock<Config> = LazyLock::new(|| {
-    let public_root_url = std::env::var("PUBLIC_ROOT_URL").unwrap_or_else(|_| "http://localhost".to_string());
-    let host = std::env::var("APPLICATION_HOST").unwrap_or_else(|_| "0.0.0.0".to_string());
-    let port = std::env::var("APPLICATION_PORT").unwrap_or_else(|_| "3000".to_string());
+    let public_root_url =
+        env::var("PUBLIC_ROOT_URL").unwrap_or_else(|_| "http://localhost".to_string());
+    let host = env::var("APPLICATION_HOST").unwrap_or_else(|_| "0.0.0.0".to_string());
+    let port = env::var("APPLICATION_PORT").unwrap_or_else(|_| "3000".to_string());
     let secret = env::var("JWT_SECRET").unwrap_or_else(|_| DEVELOPMENT_ENCRYPTION_KEY.to_string());
 
     Config {

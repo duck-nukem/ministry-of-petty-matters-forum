ca93d67e71bd9ffa707566da3304c298bedc1009
Alex Szabo <kreatemore@gmail.com>
2025-05-31 18:13:47 +0200

    fix runtime sql error

--- src/persistence/rdbms.rs
+++ src/persistence/rdbms.rs
@@ -1,4 +1,4 @@
-use sea_orm::{EntityOrSelect, FromQueryResult, Order, QueryFilter, QueryOrder, QuerySelect};
+use sea_orm::{FromQueryResult, Order, QueryFilter, QueryOrder, QuerySelect};
 use sea_orm::{Condition, DatabaseConnection, DeriveColumn, EntityTrait, EnumIter, Select};
 use sea_orm::sea_query::Expr;
 use crate::persistence::repository::ListParameters;
@@ -22,18 +22,18 @@ where
     let resulting_rows = select.filter(condition);
     // Workaround: .count() is ambiguous, it wants to use an iterable count
     let count = resulting_rows.clone()
-        .select()
-        .column_as(Expr::col("*").count(), "count")
+        .select_only()
+        .column_as(Expr::val(1).count(), "count")
         .into_values::<_, Counter>()
         .one(db)
         .await?;
-    let final_count: u64 = count.unwrap_or_default();
+    let final_count: i64 = count.unwrap_or_default();
     let data = resulting_rows
         .offset(Some(list_parameters.calculate_offset() as u64))
         .limit(Some(list_parameters.calculate_limit() as u64))
         .order_by(ordering.0, ordering.1)
         .all(db)
         .await?;
-    
-    Ok((final_count, data))
+
+    Ok((final_count as u64, data))
 }

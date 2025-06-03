38f7b478326921b39cfb96e13bc89392d3ab21f3
Alex Szabo <kreatemore@gmail.com>
2025-06-03 21:28:29 +0200

    temporary initial deployment script

---
+++ infrastructure/initial_deploy.sh
@@ -0,0 +1,7 @@
+# assumes docker, doctl, and tofu are installed and configured
+tofu apply -target=digitalocean_container_registry.container_registry -auto-approve
+docker build --platform=linux/amd64 -t registry.digitalocean.com/petty-matters/ministry:latest ..
+docker build --platform=linux/amd64 -t registry.digitalocean.com/petty-matters/ministry:deps ../migration
+docker push registry.digitalocean.com/petty-matters/ministry:latest
+docker push registry.digitalocean.com/petty-matters/ministry:deps
+tofu apply -auto-approve

d94adc914d6a22a801aef480b5388d55111730ae
Alex Szabo <kreatemore@gmail.com>
2025-06-05 22:47:54 +0200

    alter deploy script

--- infrastructure/initial_deploy.sh
+++ infrastructure/initial_deploy.sh
@@ -1,7 +1,7 @@
 # assumes docker, doctl, and tofu are installed and configured
 tofu apply -target=digitalocean_container_registry.container_registry -auto-approve
-docker build --platform=linux/amd64 -t registry.digitalocean.com/petty-matters/ministry:latest ..
-docker build --platform=linux/amd64 -t registry.digitalocean.com/petty-matters/ministry:deps ../migration
+cd .. && docker buildx build --platform=linux/amd64 -t registry.digitalocean.com/petty-matters/ministry:latest .
+cd migration docker buildx build --platform=linux/amd64 -t registry.digitalocean.com/petty-matters/ministry:deps .
 docker push registry.digitalocean.com/petty-matters/ministry:latest
 docker push registry.digitalocean.com/petty-matters/ministry:deps
 tofu apply -auto-approve

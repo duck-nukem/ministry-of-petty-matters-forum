ef95cc2054ef64839fd76c5eafc57e5a86c3ddd9
Alex Szabo <kreatemore@gmail.com>
2025-06-05 22:47:12 +0200

    some startup "logging" to help debug startup problems on remote environments

--- src/main.rs
+++ src/main.rs
@@ -30,26 +30,35 @@ static MAIN_ENTRY_POINT: &str = "/petty-matters";
 #[tokio::main]
 #[allow(clippy::expect_used)]
 async fn main() {
+    println!("Starting up");
     let mut connection_options = ConnectOptions::new(APP_CONFIG.database_url.clone());
     connection_options.max_connections(200)
-        .min_connections(20)
+        .min_connections(5)
+        .max_connections(20)
         .connect_timeout(Duration::from_secs(5))
         .idle_timeout(Duration::from_secs(30))
         .sqlx_logging(false);
 
-    let db: DatabaseConnection = Database::connect(connection_options).await.expect("DB connect failed");    let (tx, rx) = channel(100);
-    let write_queue = Arc::new(WriteQueue::new(tx.clone()));
+    println!("Attempting to connect to the database");
+    let db: DatabaseConnection = Database::connect(connection_options).await.expect("DB connection failed");    
+    let (tx, rx) = channel(100);
+    println!("DB Connection established");
 
+    println!("Wiring services");
+    let write_queue = Arc::new(WriteQueue::new(tx.clone()));
     let topic_repository = Arc::new(RdbmsRepository::<TopicDbModel>::new(db.clone()));
     let comment_repository = Arc::new(RdbmsRepository::<CommentDbModel>::new(db.clone()));
     tokio::spawn(start_write_worker(rx, topic_repository.clone(), comment_repository.clone()));
     let topic_service = Arc::new(PettyMattersService::new(topic_repository, comment_repository, write_queue));
+    println!("Service configuration done");
 
+    println!("Configuring routes and middlewares");
     let app = Router::new()
         .route("/", get(|| async { Redirect::to(MAIN_ENTRY_POINT) }))
         .nest("/auth", auth_router())
         .nest(MAIN_ENTRY_POINT, topics_router(topic_service))
         .nest_service("/assets", ServeDir::new("assets"));
+    println!("Done; attempting to start the server");
     let address = APP_CONFIG.get_address();
     let listener = tokio::net::TcpListener::bind(&address)
         .await
